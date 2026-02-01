f660b61a91c063a279979fa056a8ef121c76934c
kanggui <kg.he@outlook.com>
2026-02-01 20:23:11 +0800

    feat: change theme to CatppuccinMacchiato

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() -> iced::Result {
     iced::daemon(App::boot, App::update, App::view)
         .title("Heart Rate View")
         .subscription(App::subscription)
+        .theme(App::theme)
         .style(|_, theme| iced::theme::Style {
             background_color: iced::Color::TRANSPARENT,
             text_color: theme.palette().text,
